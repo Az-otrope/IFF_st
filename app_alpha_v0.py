199b1adfb3ad0644ad08598b5bb401b44685e9a2
Nguyen Pham <miu@Nguyens-MacBook-Pro.local>
2023-05-03 01:12:24 -0700

    update

--- app_alpha_v0.py
+++ app_alpha_v0.py
@@ -14,15 +14,74 @@ warnings.filterwarnings("ignore")
 
 
 # DASHBOARD
-st.header('Sparkle Too Data Analysis')
-add_sidebar = st.sidebar.selectbox('Project', ('Boost','Pivot In-pack', 'Pivot On-seed'))
+add_sidebar = st.sidebar.selectbox('Project', ('Boost','Pivot In-pack','Pivot On-seed'))
                                   
                                   
 # PROJECT: Pivot In-pack   
 if add_sidebar == 'Pivot In-pack':
-    st.subheader('Pivot In-pack Data Dashboard')
+    st.title('Pivot In-pack Data Dashboard')
+    st.subheader('New Sample Information Data Entry')
+   
+    with st.form('sample_form'):
+        # input:
+        sample_id = st.text_input('FD Sample ID')
+        run_id = st.text_input('FD Run ID')
+        strain = st.selectbox('Strain', ['','Klebsiella variicola','Kosakonia sacchari'])
+        
+        eft_date = st.date_input('EFT date')
+        broth_id = st.text_input('Broth ID')
+        scale = st.selectbox('Fermentation Scale', ['','14L','150K'])
+        ferm_cond = st.text_input('Ferm condition')
+        eft = st.number_input('EFT (hr)')
+        titer = st.number_input('Broth titer (CFU/mL)')
+        age = st.number_input('Broth age (day)')
+        
+        pellet_date = st.date_input('Pelletization date')
+        cryo = st.selectbox('Cryo mix', ['','DSR','PVT70%','SKP'])
+        igd_1 = st.selectbox('Ingredient 1', ['','40% Sucrose','45.5% Sucrose'])
+        igd_2 = st.selectbox('Ingredient 2', ['','8% KH2PO4','10% Maltodextrin','22.75% Inulin'])
+        igd_3 = st.selectbox('Ingredient 3', ['','10.2% K2HPO4','0.5% MgSO4'])
+        igd_4 = st.selectbox('Ingredient 4', [''])
+        cryo_rate = st.number_input('Cryo mix addition rate')
+        
+        fd_start = st.date_input('FD start date')
+        fd_recipe = st.text_input('FD cycle recipe')
+        fd_p = st.text_input('FD pressure (mTorr)')
+        fd_time = st.number_input('FD run time (hr)')
+        rate = st.number_input('Primary ramp rate (C/min)')
+        
+        receive_date = st.date_input('PA receive date')
+        look = st.text_input('Dried appearance')
+        container = st.selectbox('Container', ['', 'Foil pouch'])
+        water_act = st.number_input('Water activity')
+        viability = st.number_input('Viability (CFU/g)')
+        
+        #yield
+        #log loss
+        storage = st.checkbox('Storage tracking')
+        seed = st.checkbox('Seed treatment')
+        note = st.text_input('Note')
+        
     
-    pivot_in_pack()
+        submitted = st.form_submit_button('Submit')
+       
+    
+    st.subheader('CFU Plating Data')
+    
+    # upload data
+    data=upload_dataset('Upload .csv file')
+    
+    if len(data) > 0:
+        raw, clean = pivot_in_pack(data)
+        
+        # display the df
+        st.write('CFU Plating data')
+        st.dataframe(raw.head())
+        st.write(f"DataFrame size: {len(raw)}")
+        
+        st.write('Processed CFU Plating Data')
+        st.dataframe(clean)
+        st.write(f"DataFrame size: {len(clean)}")
     
 
 
@@ -30,7 +89,7 @@ if add_sidebar == 'Pivot In-pack':
 
 #pivot On-seed
 if add_sidebar == 'Pivot On-seed':
-    st.subheader('Pivot On-seed Data Dashboard')
+    st.title('Pivot On-seed Data Dashboard')
     
     data=upload_dataset('Upload Raw CFU .csv file')
         
@@ -48,7 +107,7 @@ if add_sidebar == 'Pivot On-seed':
 
 #boost
 if add_sidebar == 'Boost':
-    st.subheader('Boost Data Dashboard')
+    st.title('Boost Data Dashboard')
     
     data=upload_dataset('Upload Raw CFU .csv file')
     
--- pivot_in_pack.py
+++ pivot_in_pack.py
@@ -13,74 +13,61 @@ import numpy as np
 from utils import upload_dataset
 
 
-def pivot_in_pack():
+def pivot_in_pack(df):
     
-    # Upload raw CFU data
-    st.write('Experimental CFU data')
-    rawcfu_df=upload_dataset('Upload Raw CFU .csv file')
+    df.columns = df.iloc[1]
+    df = df.iloc[2:]
+    df = df.reset_index()
+    # remove rows with NaN in 'Batch" col
+    df.dropna(subset=['Batch'],inplace=True)
+    # keep relevant cols
+    df = df[['Batch','Sample Description','Storage form','Temperature-Celsius',
+                           'T0','Date','CFU/mL','CFU/g','CV','Water Activity']]
     
-    # data cleaning and processing
-    if len(rawcfu_df) >0:
-        rawcfu_df.columns = rawcfu_df.iloc[1]
-        rawcfu_df = rawcfu_df.iloc[2:]
-        rawcfu_df = rawcfu_df.reset_index()
-        # remove rows with NaN in 'Batch" col
-        rawcfu_df.dropna(subset=['Batch'],inplace=True)
-        # keep relevant cols
-        rawcfu_df = rawcfu_df[['Batch','Sample Description','Storage form','Temperature-Celsius',
-                               'T0','Date','CFU/mL','CFU/g','CV','Water Activity']]
+    # convert to datetime for T0 and Date
+    df[['T0','Date']] = df[['T0','Date']].apply(pd.to_datetime, format="%m/%d/%y")
+    
+    # time point by days and weeks of plating
+    ## by days
+    df['Time point (day)'] = (df['Date']-df['T0']).apply(lambda x: x.days)
+    ## by weeks
+    to_week = df[['T0','Date']]
+    for i in to_week.columns:
+        to_week[i] = to_week[i].apply(lambda x:x.week)
+    df['Time point (week)'] = to_week['Date'] - to_week['T0']    
+    
+    # remove % sign for CV values while ignoring invalid values
+    for idx, row in df.iterrows():
+        try:
+            df.loc[idx, "CV"] = float(row['CV'].split("%")[0])
+        except Exception as e:
+            pass
+    
+    # handle invalid values and change to float
+    to_float = df[['CFU/mL','CFU/g','CV','Water Activity']]
+    for col in to_float.columns:
+        df[col] = df[col].replace('#DIV/0!', np.NaN)
+        df[col] = df[col].astype(float)      
         
-        # convert to datetime for T0 and Date
-        rawcfu_df[['T0','Date']] = rawcfu_df[['T0','Date']].apply(pd.to_datetime, format="%m/%d/%y")
-        
-        # calculate the time point by days and weeks of plating
-        ## by days
-        rawcfu_df['Time point (day)'] = (rawcfu_df['Date']-rawcfu_df['T0']).apply(lambda x: x.days)
-        ## by weeks
-        to_week = rawcfu_df[['T0','Date']]
-        for i in to_week.columns:
-            to_week[i] = to_week[i].apply(lambda x:x.week)
-        rawcfu_df['Time point (week)'] = to_week['Date'] - to_week['T0']    
-        
-        # remove percentage sign for CV values while ignoring invalid values
-        for idx, row in rawcfu_df.iterrows():
-            try:
-                rawcfu_df.loc[idx, "CV"] = float(row['CV'].split("%")[0])
-            except Exception as e:
-                pass
-        
-        # handle invalid values and change to float
-        to_float = rawcfu_df[['CFU/mL','CFU/g','CV','Water Activity']]
-        for col in to_float.columns:
-            rawcfu_df[col] = rawcfu_df[col].replace('#DIV/0!', np.NaN)
-            rawcfu_df[col] = rawcfu_df[col].astype(float)      
-            
-        # change col names
-        rawcfu_df.rename(columns={'Batch':'FD Run ID', 'Temperature-Celsius':'Temperature (C)', 'CV':'CV (%)'}, inplace=True)
-        
-        # Record the CFUs by week for each ID
-        pivot_rawcfu = rawcfu_df.pivot(index='FD Run ID', columns='Time point (week)', values=['CFU/mL','CFU/g'])
-        # rename the column by the counting week
-        pivot_rawcfu.columns = [f"W{week}_{scale}" for scale, week in pivot_rawcfu.columns.to_list()]
-        
-        # remove cols that cause repeated samples
-        cfu = rawcfu_df.drop(['T0','Date','CFU/mL','CFU/g','CV (%)','Time point (day)','Time point (week)'],axis=1)
-        # drop duplicated IDs
-        cfu.drop_duplicates(subset='FD Run ID', inplace=True)
-        
-        # join the pivot df with the original info
-        cleaned_cfu = pd.merge(cfu, pivot_rawcfu, on='FD Run ID')
-
-        # display the df
-        st.write('CFU Plating Data')
-        st.dataframe(rawcfu_df.head())
-        st.write(f"DataFrame size: {len(rawcfu_df)}")
-        
-        st.write('Processed CFU Plating Data')
-        st.dataframe(cleaned_cfu)
-        st.write(f"DataFrame size: {len(cleaned_cfu)}")
-        
-        return rawcfu_df, cleaned_cfu
+    # change col names
+    df.rename(columns={'Batch':'FD Run ID', 'Temperature-Celsius':'Temperature (C)', 'CV':'CV (%)'}, inplace=True)
+    
+    # Record the CFUs by week for each ID
+    pivot_rawcfu = df.pivot(index='FD Run ID', columns='Time point (week)', values=['CFU/mL','CFU/g'])
+    # rename the cols by the counting week
+    pivot_rawcfu.columns = [f"W{week}_{scale}" for scale, week in pivot_rawcfu.columns.to_list()]
+    
+    # remove cols that cause duplicated samples
+    cfu = df.drop(['T0','Date','CFU/mL','CFU/g','CV (%)','Time point (day)','Time point (week)'],axis=1)
+    # drop duplicated IDs
+    cfu.drop_duplicates(subset='FD Run ID', inplace=True)
+    
+    # join the pivot df with the original info
+    cleaned_cfu = pd.merge(cfu, pivot_rawcfu, on='FD Run ID')
+    
+    # to keep: df - original data with minimal cleaning
+    # to keep: cleaned_cfu - processed data for further analysis
+    return df, cleaned_cfu
     
     
    
