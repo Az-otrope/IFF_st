6cfacab10b10e60a30466028a5f46a28a2d329ec
Nguyen Pham <miu@Nguyens-MacBook-Pro.local>
2023-06-01 14:36:56 -0700

    update web app

---
+++ home_page.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def homepage():
+    st.title('Sparkle Too Data Analysis')
+    
+    st.write('''
+            This web application is a data analysis and management platform for Sparkle Too portfolio. 
+            
+            Users can navigate to pages from the left sidebar: 
+                
+                * Master Sample Registration
+                * In-pack
+                * On-seed
+                * Boost
+                
+            Each project page allows users to perform various actions to interact with the database such as input sample's information, upload experimental data, and
+            retreive reports for further analysis.
+            
+            Start exploring and using this platform by selecting a project for detailed instructions.
+            
+            Have fun and keep experimenting!
+            
+                ''')
+                
+    st.info('Contact Nguyen @nguyen.pham@iff for feedback and questions')
+    st.balloons()
