49444f3304db10237275cd7c47b982834d9f57f5
Nguyen Pham <miu@Nguyens-MacBook-Pro.local>
2023-06-14 23:38:23 -0700

    fix typo

--- pivot_in_pack.py
+++ pivot_in_pack.py
@@ -132,6 +132,8 @@ def pivot_in_pack_app():
             df_v1['Date'].max().date(),
             value=(df_v1['T0'].min().date(), df_v1['Date'].max().date()),
             format='YYYY/MM/DD'
+        )
+
 
         df_v1_show = df_v1[(df_v1['T0'] >= pd.Timestamp(exp_period[0])) & (df_v1['Date'] <= pd.Timestamp(exp_period[1]))]
         st.dataframe(df_v1_show)
